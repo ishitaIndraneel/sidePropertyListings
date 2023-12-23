5271c20f4ce7429accb3bfbc0a85ef19b5f5866a
ishitaIndraneel <ishi.ch@gmail.com>
2023-12-23 16:51:22 -0600

    Update propertyListing.js

--- src/components/property/propertyListing.js
+++ src/components/property/propertyListing.js
@@ -62,33 +62,32 @@ const PropertyListing = () => {
     }
     return (
         <>
-        <Grid container className="container" rowSpacing={3} >
-
-                   
-                    {
+        <Grid container  className="container" spacing={0} direction="row"  >
+            <Grid item xs={4} sm={4} lg={2} md={2}></Grid>
+            <Grid item xs={4} sm={4} lg={9} md={8}>
+                <Grid container>
+                {
                         listings.map((listing)=>{
                         return (
-                            <Grid className="alignImage" item key={listing.mlsId} sm={12} xl={4} lg={4} md={5}>
-
+                            <Grid  className="card" item key={listing.mlsId} xs={12} sm={12} xl={4} lg={5} md={8}>
                                    <div className="property_details_container" >
-                                        <img className="property_pics" src={listing.photos[0]}  alt="listing" />
                                         <div onClick={()=>storeFav(listing.mlsId)}className="heart_icon">
-                                            {
-                                                isListingFav(listing.mlsId) ? 
-                                                <img src={heartFill}  alt="favorite icon" />
-                                                :
-                                                <img src={heartStroke}  alt="unfavorite icon" />
-                                            }
-                                        </div>
-                            
-                                        <div className="bed_bath">
-                                            <span>{listing.property.bedrooms} BR | </span>
-                                            <span>{`${listing.property.bathsFull}${listing.property.bathsHalf > 0 ? '.5': ''}`} BR | </span>
-                                            <span>{listing.property.area} Sq Ft</span>
-                                        </div>
-                                        <div className="home_price">${getFormattedPrice(listing.listPrice)}</div>
-                                        <div className="address">{listing.address.streetNumber} {listing.address.streetName}, {listing.address.city}, {listing.address.state}</div>
-                                        <div className="date">{getListingDate(listing.listDate)}</div>
+                                                    {
+                                                        isListingFav(listing.mlsId) ? 
+                                                        <img src={heartFill}  alt="favorite icon" />
+                                                        :
+                                                        <img src={heartStroke}  alt="unfavorite icon" />
+                                                    }
+                                            </div>
+                                            <img className="property_pics" src={listing.photos[0]}  alt="listing" />
+                                            <div className="bed_bath">
+                                                <span>{listing.property.bedrooms} BR | </span>
+                                                <span>{`${listing.property.bathsFull}${listing.property.bathsHalf > 0 ? '.5': ''}`} BR | </span>
+                                                <span>{listing.property.area} Sq Ft</span>
+                                            </div>
+                                            <div className="home_price">${getFormattedPrice(listing.listPrice)}</div>
+                                            <div className="address">{listing.address.streetNumber} {listing.address.streetName}, {listing.address.city}, {listing.address.state}</div>
+                                            <div className="date">Listed: {getListingDate(listing.listDate)}</div>
                                     </div>
                                   
 
@@ -96,9 +95,9 @@ const PropertyListing = () => {
                         )
                         })
                     }
-                    
-               
-
+                </Grid>
+            </Grid>
+            <Grid item sm={4} xs={4} lg={1} md={2}></Grid>
         </Grid>
         </>
         
